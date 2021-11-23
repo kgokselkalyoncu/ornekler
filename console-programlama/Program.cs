23d90d6180af394e52432668d7485d2b91f69bc9
Kazım Göksel KALYONCU <goksel.kalyoncu@gmail.com>
2021-11-23 22:49:58 +0300

    Ornekler ilk commit

---
+++ console-programlama/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace console_programalama
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            string? name = "";
+            string? surname = "";
+
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Please Name enter");
+            name = Console.ReadLine();
+            Console.WriteLine("Please Surname enter");
+            surname = Console.ReadLine();
+
+            Console.WriteLine("Merhaba " + name + " " + surname);
+        }
+    }
+}
