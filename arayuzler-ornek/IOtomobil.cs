2e9103d33efae7bd701e907e378ba0235bab5750
Kazım Göksel KALYONCU <goksel.kalyoncu@gmail.com>
2022-01-02 13:46:03 +0300

    Arayüz örnek kodları

---
+++ arayuzler-ornek/IOtomobil.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace arayuzler_ornek
+{
+    public interface IOtomobil{
+        int KacTekerlektenOlusur();
+        
+        Marka HangiMarkaninAraci();
+
+        Renk StandartRengiNe();
+    }
+}
